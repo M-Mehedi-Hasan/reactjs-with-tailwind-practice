ed85036cbc5b0c4aab5f4db5641fd354b032db30
M-Mehedi-Hasan <s5mdmehedihossain@gmail.com>
2022-11-11 16:47:39 +0600

    third commit

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,10 +3,10 @@ import Button from "./Button"
 
 const CTA = () => {
   return (
-    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} flex-col sm:flex-row bg-black-gradient-2 box-shadow rounded-[20px]`}>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} flex-col sm:flex-row bg-black-gradient-2 box-shadow rounded-[20px] object-contain`}>
       <div className="flex flex-1 flex-col">
         <h2 className={`${styles.heading2}`}>Let’s try our service now!</h2>
-        <p className={`${styles.paragraph} w-[445px] h-[58px] leading-[29px] mt-6`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+        <p className={`${styles.paragraph} leading-[29px] mt-6`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
       </div>
       <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
         <Button />
